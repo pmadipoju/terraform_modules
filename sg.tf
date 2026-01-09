85ba16bf47def85b21b761bf57439b3bb12c8522
pmadipoju <133623577+pmadipoju@users.noreply.github.com>
2026-01-09 16:03:23 +0530

    add

---
+++ sg.tf
@@ -0,0 +1,59 @@
+resource "aws_security_group" "aws_import_sg" {
+    lifecycle {
+      ignore_changes = all
+    }
+    description = "default VPC security group"
+    egress      = [
+        {
+            cidr_blocks      = [
+                "0.0.0.0/0",
+            ]
+            description      = null
+            from_port        = 0
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "-1"
+            security_groups  = []
+            self             = false
+            to_port          = 0
+        },
+    ]
+
+    ingress     = [
+        {
+            cidr_blocks      = [
+                "0.0.0.0/0",
+            ]
+            description      = null
+            from_port        = 22
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "tcp"
+            security_groups  = []
+            self             = false
+            to_port          = 22
+        },
+        {
+            cidr_blocks      = [
+                "0.0.0.0/0",
+            ]
+            description      = null
+            from_port        = 80
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "tcp"
+            security_groups  = []
+            self             = false
+            to_port          = 80
+        },
+    ]
+    name        = "default"
+    name_prefix = null
+    region      = "ap-south-1"
+    tags        = {
+        Name = "default"
+        Owner = "default"
+    }
+    tags_all    = {}
+    vpc_id      = "vpc-0a9d21ddd5e3ae98d"
+}
